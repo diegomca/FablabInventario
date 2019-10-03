0a6e2d175a032da22b8c23c92a5cfb2cba472d61
Diego Monsalves <diego.monsalves@alumnos.uv.cl>
2019-10-03 01:08:48 -0300

    Inicio de logeo con api

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react';
 import Barrasuperior from './Barrasuperior'
 import BackgroundSlider from 'react-background-slider'
@@ -7,81 +7,79 @@ import image2 from '../img/login/slide-img-2.jpg'
 import image3 from '../img/login/slide-img-3.png'
 import './Login.css'
 //import { Redirect } from 'react-router-dom'
-import './Login.css';
 
-class Login extends React.Component {
 
-    constructor(props) {
-        super(props);
+function Login() {
 
-        this.state = {
-            username: '',
-            password: '',
-            //submitted: false,
-            loading: false,
-            //error: ''
-        };
+    const [usersApi, setUsersApi] = useState("");
+    const [userNick, setUserNick] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    //const [hasError, setErrors] = useState(false)
 
-    }
+    useEffect(() => {
 
-/*
-    handleSubmit(e) {
-        e.preventDefault();
-
-        this.setState({ submitted: true });
-        const { username, password, returnUrl } = this.state;
-
-        // stop here if form is invalid
-        if (!(username && password)) {
-            return;
+        async function fetchData() {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users");
+            res
+                .json()
+                .then(res => setUsersApi(res))/*
+                .catch(err => setErrors(err))*/;
         }
-
-        
+        fetchData();
+    });
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        alert(`Usuario: ${userNick} \nContraseña: ${userPassword}`)
+        setUserNick('');
+        setUserPassword('');
     }
-*/
-    render() {
-        const { username, password, /*submitted, error*/ } = this.state;
-        return (
-            <div>
-                <Barrasuperior />
-                <div className="Login">
-                    <BackgroundSlider images={[image2, image1, image3]}
-                        duration={5} transition={2} />
-                    <Grid textAlign="center" verticalAlign="middle">
-                        <Grid.Column style={{ maxWidth: 400 }}>
-                            <Segment>
-                                <Header as="h2" textAlign="center">
-                                    Ingreso a Fablab Inventario
+
+    return (
+        <div>
+            <Barrasuperior />
+            <div className="Login">
+                <BackgroundSlider images={[image2, image1, image3]}
+                    duration={5} transition={2} />
+                <Grid textAlign="center" verticalAlign="middle">
+                    <Grid.Column style={{ maxWidth: 400 }}>
+                        <Segment>
+                            <Header as="h2" textAlign="center">
+                                Ingreso a Fablab Inventario
                         </Header>
-                                <Form size="large" onSubmit={this.handleSubmit} >
-                                    <Form.Input
-                                        fluid
-                                        icon="user"
-                                        iconPosition="left"
-                                        placeholder="Usuario"
-                                        value={username}
-                                    />
+                            <Form size="large" >
+                                <Form.Input
+                                    fluid
+                                    icon="user"
+                                    iconPosition="left"
+                                    placeholder="Usuario"
+                                    value={userNick}
+                                    onChange={e => setUserNick(e.target.value)}
+                                />
 
-                                    <Form.Input
-                                        fluid
-                                        icon="lock"
-                                        iconPosition="left"
-                                        placeholder="Contraseña"
-                                        type="password"
-                                        value={password}
-                                    />
+                                <Form.Input
+                                    fluid
+                                    icon="lock"
+                                    iconPosition="left"
+                                    placeholder="Contraseña"
+                                    type="password"
+                                    onChange={e => setUserPassword(e.target.value)}
+                                    value={userPassword}
+                                />
 
-                                    <Button color="instagram" fluid size="large"  >
-                                        Ingresar
+                                <Button color="instagram" fluid size="large" type="submit" onClick={handleSubmit} >
+                                    Iniciar Sesión
                                 </Button>
 
-                                </Form>
-                            </Segment>
-                        </Grid.Column>
-                    </Grid>
-                </div>
+                            </Form>
+                        </Segment>
+
+                    </Grid.Column>
+                </Grid>
             </div>
-        );
-    }
+            <div>
+                {/*JSON.stringify(usersApi)*/}
+            </div>
+        </div>
+    );
 }
-export default Login ;
+export default Login;
